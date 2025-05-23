e38ed468cbff6c039f9a139d211b3da70819f793
Khanhlam1186 <khanhyt2002@gmail.com>
2025-05-23 10:58:03 +0700

    mk

--- api/products.js
+++ api/products.js
@@ -1,9 +1,8 @@
 import path from 'path';
 import { promises as fs } from 'fs';
 
-export default async function handler(req, res) {
+export async function getProducts() {
   const jsonDirectory = path.join(process.cwd(), 'data');
   const fileContents = await fs.readFile(jsonDirectory + '/products.json', 'utf8');
-  const products = JSON.parse(fileContents);
-  res.status(200).json(products);
+  return JSON.parse(fileContents);
 }
